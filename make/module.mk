c2fb77554d990f6b42f45fdd03f129a98b68af03
Lixing Ding <ding.lixing@gmail.com>
2017-05-12 16:38:29 +0800

    update build scripts of module

--- make/module.mk
+++ make/module.mk
@@ -35,7 +35,7 @@ elf_NAMES = cupkee
 cupkee_SRCS = ${notdir ${wildcard ${MAIN_DIR}/*.c}}
 cupkee_CPPFLAGS = -I${INC_DIR} -I${LANG_DIR}/include
 cupkee_CFLAGS   =
-cupkee_LDFLAGS  = -L${BUILD_DIR} -L${BSP_BUILD_DIR} -L${SYS_BUILD_DIR} -L${LANG_BUILD_DIR} -lsys -lbsp -llang -lmodule
+cupkee_LDFLAGS  = -L${BUILD_DIR} -L${BSP_BUILD_DIR} -L${SYS_BUILD_DIR} -L${LANG_BUILD_DIR} -lmodule -lsys -lbsp -llang
 
 include ${MAKE_DIR}/cupkee.ruls.mk
 
